b83f35967ee2ffdf613e96ee9608a8b2b6cb5875
Ayush Prakash <ayushprakash800@gmail.com.com>
2024-09-25 15:24:27 +0530

    third commit

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,14 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+app.get("/",(req,res)=>{
+    res.json({
+        success:true,
+        message:"Welcome my friend",
+    });
+});
+
 app.use('/api/v1/reservation', reservationRouter);
 
 
